90db886ebf0ac5a1d2c229ec7c823e3d6b7bb2b4
pea84 <tea84@rambler.ru>
2022-08-06 09:08:59 +0300

    Example5. string

---
+++ 01 Lection 1/Example005/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string un = Console.ReadLine();
+if (un.ToLower()=="Маша")
+{Console.WriteLine("Привет, Маша");}
+else
+{Console.Write("Привет,");
+Console.WriteLine(un);}
