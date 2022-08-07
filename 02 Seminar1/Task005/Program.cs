3f422e22c7633d75cfdc799c346a30c0026d6b1e
pea84 <tea84@rambler.ru>
2022-08-07 16:29:48 +0300

    task005. Division

---
+++ 02 Seminar1/Task005/Program.cs
@@ -0,0 +1,10 @@
+﻿// Вывод последнией цифры 3х значного числа
+Console.WriteLine("Введите число >=100 и <1000....");
+
+int N = Convert.ToInt32(Console.ReadLine());
+if ((N >= 100) && (N < 1000))
+    {
+        int r = N % 10;
+        Console.WriteLine($"Последняя цифра равна...{r}");
+    }
+else  Console.WriteLine($"Некорректное число...{N}");       
