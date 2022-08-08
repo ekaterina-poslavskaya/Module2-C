83721e5027763d12decc90d584d359fa23ebd18c
pea84 <tea84@rambler.ru>
2022-08-08 08:19:55 +0300

    after rename, remove

---
+++ 02 Seminar1/Task001/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a == b*b) 
+    {Console.Write("Число а=");
+    Console.Write(a);
+    Console.Write(" является квадратом числа b=");
+    Console.Write(b);}
