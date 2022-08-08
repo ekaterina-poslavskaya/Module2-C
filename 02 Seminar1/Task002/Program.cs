cc1bf9e04c9dce0cf3515a5353afdeaf74d55c3c
pea84 <tea84@rambler.ru>
2022-08-08 08:50:28 +0300

    Homework. Task002. Max Number

---
+++ 02 Seminar1/Task002/Program.cs
@@ -0,0 +1,12 @@
+﻿// ДЗ. Напишите программу, 
+// которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее
+// 
+Console.Write("Введите целое число а=");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число b=");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a>b) {Console.Write($"Число а={a} больше числа b={b}; max={a}");}
+else if (a<b) {Console.Write($"Число а={a} меньше числа b={b}; max={b}");}
+else {Console.Write($"Числа а={a} и b={b} равны");}
