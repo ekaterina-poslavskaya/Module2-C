4bf5dc8af246feb8afe5ec1a00d9d127189a82b4
pea84 <tea84@rambler.ru>
2022-08-08 08:38:34 +0300

    task003

--- 02 Seminar1/Task003/Program.cs
+++ 02 Seminar1/Task003/Program.cs
@@ -1,8 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int a = Convert.ToInt32(Console.ReadLine());
-int b = Convert.ToInt32(Console.ReadLine());
-if (a == b*b) 
-    {Console.Write("Число а=");
-    Console.Write(a);
-    Console.Write(" является квадратом числа b=");
-    Console.Write(b);}
+Console.Write("Введите число от 1 до 7...");
+int i = Convert.ToInt32(Console.ReadLine());
+if (i==1) {Console.WriteLine("Понедельник");}
+else if (i==2) {Console.WriteLine("Вторник");}
+else if (i==3) {Console.WriteLine("Среда");}
+else if (i==4) {Console.WriteLine("Четверг");}
+else if (i==5) {Console.WriteLine("Пятница");}
+else if (i==6) {Console.WriteLine("Суббота");}
+else if (i==7) {Console.WriteLine("Воскресенье");}
+else {Console.WriteLine("Введено неправильное число");}
