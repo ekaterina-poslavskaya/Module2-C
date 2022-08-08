6ffa5e7e680d15219ecdba907aeb22348293feeb
pea84 <tea84@rambler.ru>
2022-08-08 08:33:33 +0300

    another attemp to load only needable files

---
+++ 02 Seminar1/Task000/Program.cs
@@ -0,0 +1,4 @@
+﻿// Возведение в квадрат
+string s = Console.ReadLine();
+int a = Convert.ToInt32(s);
+Console.WriteLine (a*a);
---
+++ 02 Seminar1/Task001/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a == b*b) 
+    {Console.Write($"Число а={a} является квадратом числа b={b}");
+     //Console.Write("Число а=");
+    //Console.Write(a);
+    //Console.Write(" является квадратом числа b=");
+    //Console.Write(b);
+    }
