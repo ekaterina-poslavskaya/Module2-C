a5a195ad0869bb54142a47bb08803106e65bfea2
pea84 <tea84@rambler.ru>
2022-08-08 09:12:17 +0300

    Homework. Task004. Max from 3 numbers

---
+++ 02 Seminar1/Task004/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 4: Напишите программу, 
+//      которая принимает на вход три числа и 
+//      выдаёт максимальное из этих чисел.
+
+Console.Write("Введите целое число а=");
+int max = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число b=");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число c=");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (b > max) {max = b;}
+if (c > max) {max = c;}
+
+Console.WriteLine($"Максимальное число равно {max}");
