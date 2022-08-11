0170ca1e41e3a13266502413aeeb33be56b9c513
pea84 <tea84@rambler.ru>
2022-08-11 16:37:59 +0300

    array

--- 03 Lection 2/Example010/Program.cs
+++ 03 Lection 2/Example010/Program.cs
@@ -1,4 +1,4 @@
-﻿int[] array = {11, 21, 31, 4, 51, 61, 17, 18, 19};
+﻿int[] array = {11, 21, 31, 4, 4, 61, 17, 18, 19};
 
 int n = array.Length;
 int find = 4;
@@ -7,6 +7,7 @@ while (i < n)
     {
         if (array[i] == find)
             {Console.WriteLine(i);
+            break;
             }
         i++;
     }
