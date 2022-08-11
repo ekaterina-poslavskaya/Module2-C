44cf6a91a795f096f4e23fc1808876e525bddb8d
pea84 <tea84@rambler.ru>
2022-08-11 16:56:02 +0300

    func+array

--- 03 Lection 2/Example011/Program.cs
+++ 03 Lection 2/Example011/Program.cs
@@ -21,6 +21,27 @@ void PrintArray(int[] col)
     }
 }
 
+int IndexOf(int[] collection, int find)
+{
+    int i = 0;
+    int result = -1;
+
+    while (i < collection.Length)
+    {
+        if (collection[i]==find)
+            {
+             result = i;
+             break;  
+            }
+       i++; 
+    }
+    return result;
+}
+
 int[] array = new int[10];
 FillArray(array);
 PrintArray(array);
+Console.WriteLine();
+array[7] = 4;
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
