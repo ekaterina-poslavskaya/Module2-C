30bbfadcfdd4e42371591b900805b78b0c5df3c0
pea84 <tea84@rambler.ru>
2022-08-11 18:45:43 +0300

    Task 11 with strings

--- 04 Seminar2/Task011/Program.cs
+++ 04 Seminar2/Task011/Program.cs
@@ -4,6 +4,13 @@
 */
 int num = new Random().Next(100,1000);
 Console.WriteLine(num);
-int digit1 = num/100;
+/*int digit1 = num/100;
 int digit2 = num % 10;
 Console.WriteLine(" "+digit1.ToString()+digit2.ToString());
+*/
+string str_num = num.ToString();
+char digit1 = str_num[0];
+char digit3 = str_num[2];
+string new_str_num = digit1.ToString() + digit3.ToString();
+int new_num = Convert.ToInt32(new_str_num);
+Console.WriteLine(new_num);
