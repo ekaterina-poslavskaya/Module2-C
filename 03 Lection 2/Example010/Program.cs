c34f9ad805f7ca1be8fb2fbbdff4f3590813fcf9
pea84 <tea84@rambler.ru>
2022-08-11 16:34:23 +0300

    ex9 && ex10

---
+++ 03 Lection 2/Example010/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = {11, 21, 31, 4, 51, 61, 17, 18, 19};
+
+int n = array.Length;
+int find = 4;
+int i = 0;
+while (i < n)
+    {
+        if (array[i] == find)
+            {Console.WriteLine(i);
+            }
+        i++;
+    }
