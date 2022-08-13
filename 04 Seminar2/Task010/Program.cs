99c82c41274d81fbabaa4bddc2cdde68f514e005
Ekaterina Poslavskaya <tea84@rambler.ru>
2022-08-13 09:37:12 +0300

    task 10 below zero

--- 04 Seminar2/Task010/Program.cs
+++ 04 Seminar2/Task010/Program.cs
@@ -10,10 +10,7 @@ Console.Write("Введите трехзначное число...");
 int num = Convert.ToInt32(Console.ReadLine());
 if ((Math.Abs(num)>=100)&&(Math.Abs(num) < 1000))
     {
-     string num_str = num.ToString();   
-     if (num>0)
-       Console.WriteLine($"Вторая цифра числа {num}: {num_str[1]}");
-      else 
-       Console.WriteLine($"Вторая цифра числа {num}: {num_str[2]}"); 
+     string num_str = Math.Abs(num).ToString();   
+     Console.WriteLine($"Вторая цифра числа {num}: {num_str[1]}");
     }
 else Console.WriteLine($"Число {num} не трехзначное!");    
