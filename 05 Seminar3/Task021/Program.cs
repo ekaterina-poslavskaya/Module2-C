1553622a57bbc461a4d2341a7f53c3530d0d2f15
Ekaterina Poslavskaya <tea84@rambler.ru>
2022-08-13 16:36:26 +0300

    Task021

---
+++ 05 Seminar3/Task021/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Задача 21: Напишите программу, которая принимает на вход 
+координаты двух точек и находит расстояние между 
+ними в 2D пространстве.
+A (3,6); B (2,1) -> 5,09 
+A (7,-5); B (1,-1) -> 7,21
+*/
+double VectorScalar(int xA, int yA, int xB, int yB)
+{
+ return Math.Sqrt(Math.Pow(xA-xB,2)+Math.Pow(yA-yB,2));
+
+}
+
+Console.WriteLine("{0:N2}",VectorScalar(3,6,2,1).ToString());
+Console.WriteLine("{0:N2}",VectorScalar(7,-5,1,-1).ToString());
