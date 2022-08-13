ca8913104c0d324a3f92cc36db6eae4450ea4f73
Ekaterina Poslavskaya <tea84@rambler.ru>
2022-08-13 09:06:30 +0300

    task 14

---
+++ 04 Seminar2/Task014/Program.cs
@@ -0,0 +1,9 @@
+﻿/*
+14. Напишите программу, которая принимает на вход число и проверяет, 
+кратно ли оно одновременно 7 и 23.
+*/
+
+int num = Convert.ToInt32(Console.ReadLine());
+if ((num % 7 == 0) && (num % 23 == 0))
+    Console.WriteLine($"{num} делится на 7 и на 23");
+else Console.WriteLine($"{num} НЕ делится на 7 и на 23");
