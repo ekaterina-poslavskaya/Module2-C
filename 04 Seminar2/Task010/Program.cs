14358333707ab7a33b8fd6af8e852a26cb517578
Ekaterina Poslavskaya <tea84@rambler.ru>
2022-08-13 09:28:02 +0300

    Task010

---
+++ 04 Seminar2/Task010/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход 
+трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+Console.Write("Введите трехзначное число...");
+int num = Convert.ToInt32(Console.ReadLine());
+if ((num>=100)&&(num < 1000))
+    {
+     string num_str = num.ToString();   
+     Console.WriteLine($"Вторая цифра числа {num}: {num_str[1]}");
+    }
+else Console.WriteLine($"Число {num} не трехзначное!");    
