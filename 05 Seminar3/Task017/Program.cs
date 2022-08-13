8b2c1505d053a920ba11da4ab3b47162c54bcdb6
Ekaterina Poslavskaya <tea84@rambler.ru>
2022-08-13 16:19:27 +0300

    Task 017

---
+++ 05 Seminar3/Task017/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+17. Напишите программу, которая принимает на вход 
+координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и 
+выдаёт номер четверти плоскости, в которой находится эта точка.
+*/
+int FindQuater(int x, int y)
+{
+ if ((x>0)&&(y>0)) return 1;
+ else if ((x<0)&&(y>0)) return 2;
+ else if ((x<0)&&(y<0)) return 3;
+ else if ((x>0)&&(y<0)) return 4;
+ else return -1;    
+}
+
+Console.WriteLine(FindQuater(2,3));
+Console.WriteLine(FindQuater(-2,3));
+Console.WriteLine(FindQuater(-2,-3));
+Console.WriteLine(FindQuater(2,-3));
+Console.WriteLine(FindQuater(2,0));
