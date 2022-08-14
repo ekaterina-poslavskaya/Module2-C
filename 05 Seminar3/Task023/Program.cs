07ff6d9c2edadb79fc83feb5e21321cc7f04e0dd
Ekaterina Poslavskaya <tea84@rambler.ru>
2022-08-14 09:24:02 +0300

    Homework. Task023

---
+++ 05 Seminar3/Task023/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Задача 23
+Напишите программу, которая принимает на вход число (N) 
+и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+string AllNumCoub (int num)
+{
+ string result = "";
+ if (num >= 0)
+    {
+      for (int i=0; i<=num; i++)
+        result = result + (i*i*i).ToString() + " ";   
+    }
+ else
+    { 
+      for (int i=0; i>=num; i--)
+          result = result + (i*i*i).ToString() + " ";
+    }
+
+ return result;   
+}
+
+Console.WriteLine("3: " + AllNumCoub(3));
+Console.WriteLine("5: " + AllNumCoub(5));
+Console.WriteLine("2: " + AllNumCoub(2));
+Console.WriteLine("-6: " + AllNumCoub(-6));
+Console.WriteLine("0: " + AllNumCoub(0));
