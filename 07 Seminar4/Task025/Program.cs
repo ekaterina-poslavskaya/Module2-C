d611c0e1937ef8f4dad5630def4ab281e172a0e2
Ekaterina Poslavskaya <tea84@rambler.ru>
2022-08-18 17:05:21 +0300

    Homework: task05

---
+++ 07 Seminar4/Task025/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два 
+числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+int ReadData(string comm)
+{
+    Console.Write(comm);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int LocalPow(int A, int B)
+{
+    int result = 1;
+    for (int i = 1; i <= B; i++)
+        result *= A;
+
+    return result;
+}
+
+int A = ReadData("Основание степени...");
+int B = ReadData("Показатель степени...");
+if (B < 0)
+{
+    Console.WriteLine("Показатель степени должен быть > 0");
+}
+else
+{
+    Console.WriteLine($"{A} в степени {B} = {LocalPow(A, B)}");
+
+}
