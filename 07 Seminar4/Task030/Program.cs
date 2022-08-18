b41ff70d0a8f313487b9c121136fa449e89726fb
Ekaterina Poslavskaya <tea84@rambler.ru>
2022-08-18 16:43:53 +0300

    task030

---
+++ 07 Seminar4/Task030/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача 30: Напишите программу, которая выводит массив из 
+8 элементов, заполненный нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0]
+*/
+void PrintArray(int[] A)
+{
+    Console.Write("массив: ");
+    for (int i = 0; i < A.Length; i++)
+        Console.Write($"{A[i]} ");
+}
+
+void FillArray(int[] A, int minValue, int maxValue)
+{
+    for (int i = 0; i < A.Length; i++)
+        A[i] = new Random().Next(minValue, maxValue);
+}
+
+Console.Write("Введите длину массива...");
+int size = Convert.ToInt32(Console.ReadLine());
+if (size > 0)
+{
+    int[] array = new int[size];
+    FillArray(array, 0, 2);
+    PrintArray(array);
+}
+else
+{
+    Console.WriteLine("Длина массива должна быть больше 0");
+}
