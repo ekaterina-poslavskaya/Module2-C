d7c7c668a2c4ff7a6138c7ac7c8aaffa72208e86
Ekaterina Poslavskaya <tea84@rambler.ru>
2022-08-20 01:22:51 +0300

    Homework: task036 + надоело копировать функции, вынесла в отдельный файл 8-0

---
+++ 08 Seminar5/Task036/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Задача 36: Задайте одномерный массив, заполненный 
+случайными числами. Найдите сумму элементов, 
+стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0
+*/
+
+
+using BasicArrFunc;
+namespace Task036
+{
+    public class Task036
+    {
+        private static int SumOddElements(int[] arS)
+        {
+            int res = 0;
+            for (int i = 1; i < arS.Length; i+=2)//начинаем с первого нечетного, шаг 2
+            {
+                res+=arS[i];
+            }
+            return res;
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            int[] ar = clsBasicArrFunc.InitArray(0, 1, 20);
+            if (ar.Length > 0)
+            {
+                Console.WriteLine($"Сумма элементов с нечетными индексами = {SumOddElements(ar)}");
+
+            }
+
+        }
+    }
+}
