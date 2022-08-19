0de80c6dcd697defa12de753a0735475f9eb0146
Ekaterina Poslavskaya <tea84@rambler.ru>
2022-08-20 01:41:53 +0300

    Homework: task038 + перегрузка всех методов для работы с double-массивами (в задаче требуются вещественные числа)

---
+++ 08 Seminar5/Task038/BasicArrFunc.cs
@@ -0,0 +1,128 @@
+﻿//основные методы работы с массивами
+namespace BasicArrFunc
+{
+    public class clsBasicArrFunc
+    {
+        // печать массива
+        public static void PrintArray(int[] A, string msgInBegin = "массив: ")
+        {
+            Console.Write(msgInBegin + "[");
+            for (int i = 0; i < A.Length; i++)
+                Console.Write($"{A[i]} ");
+            Console.Write("]");
+            Console.WriteLine("");
+        }
+
+        // наполнение массива значениями
+        public static void FillArray(int[] A, int minValue, int maxValue)
+        {
+            for (int i = 0; i < A.Length; i++)
+                A[i] = new Random().Next(minValue, maxValue);
+        }
+
+        //считывание данных из консоли
+        public static int ReadData(string comm = "")
+        {
+            Console.Write(comm);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        // инициализация и заполнение массива
+        public static int[] InitArray(int len, int minA = -999999999, int maxA = -999999999)
+        {
+            int size = len;
+            if (size == 0)
+            {
+                Console.Write("Введите длину массива...");
+                size = Convert.ToInt32(Console.ReadLine());
+            }
+            if (size > 0)
+            {
+                int[] array = new int[size];
+                int minRandom = minA;
+                if (minRandom == -999999999)
+                {
+                    minRandom = ReadData("Введите нижнюю границу для генерации чисел...");
+                }
+
+                int maxRandom = maxA;
+                if (maxRandom == -999999999)
+                {
+                    maxRandom = ReadData("Введите верхнюю границу для генерации чисел...");
+                }
+
+                FillArray(array, minRandom, maxRandom + 1);
+                PrintArray(array);
+                return array;
+            }
+            else
+            {
+                size = 0;//если размер меньше нуля указали или 0, то создаем нулевой массив и ругаемся
+                int[] array = new int[size];
+                Console.WriteLine("Длина массива должна быть больше 0");
+                return array;//null;
+            }
+        }
+
+        // печать массива
+        public static void PrintArray(double[] A, string msgInBegin = "массив: ")
+        {
+            Console.Write(msgInBegin + "[");
+            for (int i = 0; i < A.Length; i++)
+                Console.Write($"{A[i]} ");
+            Console.Write("]");
+            Console.WriteLine("");
+        }
+
+        // наполнение массива значениями
+        public static void FillArray(double[] A, double minValue, double maxValue)
+        {
+            for (int i = 0; i < A.Length; i++)
+                A[i] = (new Random().NextDouble()) * maxValue - minValue;
+        }
+
+        //считывание данных из консоли
+        public static double ReadDoubleData(string comm = "")
+        {
+            Console.Write(comm);
+            return Convert.ToDouble(Console.ReadLine());
+        }
+
+        // инициализация и заполнение массива
+        public static double[] InitArray(int len, double minA = -999999999, double maxA = -999999999)
+        {
+            int size = len;
+            if (size == 0)
+            {
+                Console.Write("Введите длину массива...");
+                size = Convert.ToInt32(Console.ReadLine());
+            }
+            if (size > 0)
+            {
+                double[] array = new double[size];
+                double minRandom = minA;
+                if (minRandom == -999999999)
+                {
+                    minRandom = ReadDoubleData("Введите нижнюю границу для генерации чисел...");
+                }
+
+                double maxRandom = maxA;
+                if (maxRandom == -999999999)
+                {
+                    maxRandom = ReadDoubleData("Введите верхнюю границу для генерации чисел...");
+                }
+
+                FillArray(array, minRandom, maxRandom);
+                PrintArray(array);
+                return array;
+            }
+            else
+            {
+                size = 0;//если размер меньше нуля указали или 0, то создаем нулевой массив и ругаемся
+                double[] array = new double[size];
+                Console.WriteLine("Длина массива должна быть больше 0");
+                return array;//null;
+            }
+        }
+    }
+}
---
+++ 08 Seminar5/Task038/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+Задача 38: Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.
+
+[3 7 22 2 78] -> 76
+*/
+
+using BasicArrFunc;
+namespace Task038
+{
+    public class Task038
+    {
+        private static double FindMaxElement(double[] arS)
+        {
+            double res = arS[0];
+            for (int i = 1; i < arS.Length; i++)
+            {
+                if (arS[i] > res) res = arS[i];
+            }
+            return res;
+        }
+        private static double FindMinElement(double[] arS)
+        {
+            double res = arS[0];
+            for (int i = 1; i < arS.Length; i++)
+            {
+                if (arS[i] < res) res = arS[i];
+            }
+            return res;
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            double[] ar = clsBasicArrFunc.InitArray(0, 1.0, 20.0);
+            if (ar.Length > 0)
+            {
+                double MaxE = FindMaxElement(ar);
+                double MinE = FindMinElement(ar);
+                Console.WriteLine($"Разница между макс ({MaxE}) и мин ({MinE}) = {MaxE - MinE}");
+
+            }
+
+        }
+    }
+}
