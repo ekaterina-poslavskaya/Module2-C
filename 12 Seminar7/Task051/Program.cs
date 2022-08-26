f3108658dc6fa89fc1f9358343b2ce4fa4887cc2
Ekaterina Poslavskaya <tea84@rambler.ru>
2022-08-26 17:30:08 +0300

    Task049. Пример не соответствует условию задачи. Реализация выполнена по примеру (сумма элементов главной диагонали), тк условие в точности повторяет задачу 49

---
+++ 12 Seminar7/Task051/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Сумма элементов главной диагонали: 1+9+2 = 12
+*/
+using Basic2DimArrFunc;
+
+void SumElementOnMainDiag()
+{
+    int[,] intArr = clsBasic2DimArrFunc.Init2DimIntArray(numRows: 0,
+                                                         numCols: 0,
+                                                         needPrintArray: true,
+                                                         fromKeyboard: false,
+                                                         needFillArray: true);
+
+    int cnt=0;
+    if (intArr.GetLength(0)<intArr.GetLength(1)) cnt = intArr.GetLength(0);
+    else cnt =intArr.GetLength(1);
+
+    int sumDiag = 0;
+    for (int i = 0; i < cnt; i ++)//шагаем по главной диагонали только
+        sumDiag = sumDiag + intArr[i, i];
+
+    Console.WriteLine($"Сумма элементов главной диагонали равна {sumDiag}");
+}
+
+SumElementOnMainDiag();
+
+
