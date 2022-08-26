4e26f7fd33123fe1136f6c25e9d51261b7c3e772
Ekaterina Poslavskaya <tea84@rambler.ru>
2022-08-26 16:56:58 +0300

    Task046

---
+++ 12 Seminar7/Task046/Program.cs
@@ -0,0 +1,10 @@
+﻿/*Задача 46: Задайте двумерный массив размером m×n, 
+заполненный случайными целыми числами.
+m = 3, n = 4.
+1 4 8 19
+5 -2 33 -2
+77 3 8 1
+*/
+using Basic2DimArrFunc;
+
+int[,] intArr = clsBasic2DimArrFunc.Init2DimIntArray(0,0,true,false,0,10);
