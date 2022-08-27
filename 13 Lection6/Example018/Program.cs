1a66baff7c8405e1d983a56bae961a422f0d829a
Ekaterina Poslavskaya <tea84@rambler.ru>
2022-08-28 01:26:30 +0300

    Lection 6. Кортежи

---
+++ 13 Lection6/Example018/Program.cs
@@ -0,0 +1,19 @@
+﻿// в случайных точках консоли вывести случайные числа
+
+Random R = new Random();
+
+Console.CursorVisible = false;
+int i = 0;
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Green;
+while (i < 1000)
+{
+    Console.SetCursorPosition(
+        left: R.Next(Console.WindowWidth),
+        top: R.Next(Console.WindowHeight));
+
+    Console.Write($"{R.Next(10)}\t");
+    Thread.Sleep(10);
+
+}
+Console.ResetColor();
