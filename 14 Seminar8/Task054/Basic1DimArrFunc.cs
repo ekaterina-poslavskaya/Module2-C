e4ef205b99b86ce0402f7d0a0ac9331497127dcf
Ekaterina Poslavskaya <tea84@rambler.ru>
2022-09-01 09:03:14 +0300

    Task 054. Homework. Сортировка массива построчно

---
+++ 14 Seminar8/Task054/Basic1DimArrFunc.cs
@@ -0,0 +1,159 @@
+﻿//основные методы работы с массивами
+using GeneralFunc;
+
+namespace Basic1DimArrFunc
+{
+    public class clsBasic1DimArrFunc
+    {
+        // печать массива
+        public static void PrintArray(int[] A, string msgInBegin = "массив: ")
+        {
+            Console.Write(msgInBegin + "[");
+            for (int i = 0; i < A.Length; i++)
+                Console.Write($"{A[i]} ");
+            Console.Write("]");
+            Console.WriteLine("");
+        }
+
+        // наполнение массива значениями
+        public static void FillArray(int[] A, int minValue, int maxValue)
+        {
+            for (int i = 0; i < A.Length; i++)
+                A[i] = new Random().Next(minValue, maxValue);
+        }
+
+        // инициализация и заполнение массива
+        public static int[] InitArray(int len, int minA = Int32.MinValue, int maxA = Int32.MaxValue)
+        {
+            int size = len;
+            if (size == 0)
+            {
+                size = clsGeneralFunc.ReadIntData("Введите длину массива...");
+            }
+            if (size > 0)
+            {
+                int[] array = new int[size];
+                int minRandom = minA;
+                if (minRandom == Int32.MinValue)
+                {
+                    minRandom = clsGeneralFunc.ReadIntData("Введите нижнюю границу для генерации чисел...");
+                }
+
+                int maxRandom = maxA;
+                if (maxRandom == Int32.MaxValue)
+                {
+                    maxRandom = clsGeneralFunc.ReadIntData("Введите верхнюю границу для генерации чисел...");
+                }
+
+                FillArray(array, minRandom, maxRandom + 1);
+                PrintArray(array);
+                return array;
+            }
+            else
+            {
+                size = 0;//если размер меньше нуля указали или 0, то создаем нулевой массив и ругаемся
+                int[] array = new int[size];
+                Console.WriteLine("Длина массива должна быть больше 0");
+                return array;//null;
+            }
+        }
+
+        // печать массива
+        public static void PrintArray(double[] A, string msgInBegin = "массив: ")
+        {
+            Console.Write(msgInBegin + "[");
+            for (int i = 0; i < A.Length; i++)
+                Console.Write($"{A[i]} ");
+            Console.Write("]");
+            Console.WriteLine("");
+        }
+
+        // наполнение массива значениями
+        public static void FillArray(double[] A, double minValue, double maxValue)
+        {
+            for (int i = 0; i < A.Length; i++)
+                A[i] = (new Random().NextDouble()) * maxValue - minValue;
+        }
+
+        // инициализация и заполнение массива
+        public static double[] InitArray(int len, double minA = Int32.MinValue, double maxA = Int32.MaxValue)
+        {
+            int size = len;
+            if (size == 0)
+            {
+                size = clsGeneralFunc.ReadIntData("Введите длину массива...");
+            }
+            if (size > 0)
+            {
+                double[] array = new double[size];
+                double minRandom = minA;
+                if (minRandom == Int32.MinValue)
+                {
+                    minRandom = clsGeneralFunc.ReadDoubleData("Введите нижнюю границу для генерации чисел...");
+                }
+
+                double maxRandom = maxA;
+                if (maxRandom == Int32.MaxValue)
+                {
+                    maxRandom = clsGeneralFunc.ReadDoubleData("Введите верхнюю границу для генерации чисел...");
+                }
+
+                FillArray(array, minRandom, maxRandom);
+                PrintArray(array);
+                return array;
+            }
+            else
+            {
+                size = 0;//если размер меньше нуля указали или 0, то создаем нулевой массив и ругаемся
+                double[] array = new double[size];
+                Console.WriteLine("Длина массива должна быть больше 0");
+                return array;//null;
+            }
+        }
+
+        public static void Sort1DimArray(int[] intArr,
+                                         int direction) //0 - по возрастанию
+                                                        //1 - по убыванию
+        {
+            if (direction == 0)
+            {
+                for (int j = 0; j < intArr.Length - 1; j++)
+                    for (int i = 1; i < intArr.Length - j; i++)
+                        if (intArr[i - 1] > intArr[i])
+                        {
+                            int t = intArr[i - 1];
+                            intArr[i - 1] = intArr[i];
+                            intArr[i] = t;
+                        }
+
+            } 
+            if (direction == 1)
+            {
+                for (int j = 0; j < intArr.Length - 1; j++)
+                    for (int i = 1; i < intArr.Length - j; i++)
+                        if (intArr[i - 1] < intArr[i])
+                        {
+                            int t = intArr[i - 1];
+                            intArr[i - 1] = intArr[i];
+                            intArr[i] = t;
+                        }
+
+            } 
+        }
+
+        public static int FindMinIn1DimArr(int[] arr)
+        {
+            int min = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+                if (min > arr[i]) min = arr[i];
+            return min;
+        }
+        public static int FindMaxIn1DimArr(int[] arr)
+        {
+            int max = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+                if (max < arr[i]) max = arr[i];
+            return max;
+        }
+    }
+}
---
+++ 14 Seminar8/Task054/Program.cs
@@ -0,0 +1,45 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, 
+которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+
+*/
+using Basic2DimArrFunc;
+using Basic1DimArrFunc;
+using GeneralFunc;
+
+public class clsTask54
+{
+
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("***********************************************");
+        Console.WriteLine("Задача 54. Сортировка массива по строкам");
+        Console.WriteLine("***********************************************");
+        int[,] arr = clsBasic2DimArrFunc.Init2DimIntArray(numRows: 0,
+                                                           numCols: 0
+                                                           );
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            int[] arrRow = clsBasic2DimArrFunc.GetRowAsArray(arr, i);
+            //Console.Write($"Строка {i}: ");
+            //clsBasic1DimArrFunc.PrintArray(arrRow);
+            //Console.WriteLine();
+            clsBasic1DimArrFunc.Sort1DimArray(arrRow, 1);
+            //Console.Write($"Строка {i} отсортированная: ");
+            //clsBasic1DimArrFunc.PrintArray(arrRow);
+            //Console.WriteLine();
+            clsBasic2DimArrFunc.SetArrayAsRow(arr, i, arrRow);
+        }
+        Console.WriteLine("***********************************************");
+        Console.WriteLine("Отсортированный по строкам массив");
+        clsBasic2DimArrFunc.Print2DimArray(arr);
+    }
+}
