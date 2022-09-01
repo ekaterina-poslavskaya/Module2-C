fb49180ad37bec826ddff51268aa3c5dbfc892e8
Ekaterina Poslavskaya <tea84@rambler.ru>
2022-09-01 09:14:43 +0300

    Task 056. Homework. Поиск строки с минимальной суммой элементов

---
+++ 14 Seminar8/Task056/Basic2DimArrFunc.cs
@@ -0,0 +1,215 @@
+//основные методы работы с массивами
+using GeneralFunc;
+
+namespace Basic2DimArrFunc
+{
+    public class clsBasic2DimArrFunc
+    {
+
+
+        public static int FindMinIn2DimArr(int[,] arr)
+        {
+            int min = arr[0, 0];
+            for (int i = 0; i < arr.GetLength(0); i++)
+                for (int j = 0; j < arr.GetLength(1); j++)
+                    if (min > arr[i, j]) min = arr[i, j];
+            return min;
+        }
+        private static int FindMaxIn2DimArr(int[,] arr)
+        {
+            int max = arr[0, 0];
+            for (int i = 0; i < arr.GetLength(0); i++)
+                for (int j = 0; j < arr.GetLength(1); j++)
+                    if (max < arr[i, j]) max = arr[i, j];
+            return max;
+        }
+        // печать массива
+        public static void Print2DimArray(int[,] A, string msgInBegin = "массив: ")
+        {
+            Console.WriteLine(msgInBegin);
+            for (int i = 0; i < A.GetLength(0); i++)
+            {
+                for (int j = 0; j < A.GetLength(1); j++)
+                    Console.Write($"{A[i, j]}\t");
+                Console.WriteLine();
+            }
+            Console.WriteLine("");
+        }// печать массива
+        public static void Print2DimArray(double[,] A,
+                                          string msgInBegin = "массив: ")
+        {
+            Console.WriteLine(msgInBegin);
+            for (int i = 0; i < A.GetLength(0); i++)
+            {
+                for (int j = 0; j < A.GetLength(1); j++)
+                    Console.Write($"{A[i, j]}\t");
+                Console.WriteLine();
+            }
+            Console.WriteLine("");
+        }
+
+        // наполнение массива значениями
+        public static void Fill2DimArray(int[,] A, int minValue, int maxValue)
+        {
+            for (int i = 0; i < A.GetLength(0); i++)
+                for (int j = 0; j < A.GetLength(1); j++)
+                    A[i, j] = new Random().Next(minValue, maxValue);
+        }
+        // наполнение массива значениями
+        public static void Fill2DimArray(double[,] A, double minValue, double maxValue)
+        {
+            for (int i = 0; i < A.GetLength(0); i++)
+                for (int j = 0; j < A.GetLength(1); j++)
+                    A[i, j] = new Random().NextDouble() * (maxValue - minValue) + minValue;
+        }
+        // наполнение массива значениями с клавиатуры
+        public static void Fill2DimArrayFromKeyboard(int[,] A, string message1 = "", string message2 = "")
+        {
+            if (message1 != String.Empty) Console.WriteLine(message1);
+            for (int i = 0; i < A.GetLength(0); i++)
+                for (int j = 0; j < A.GetLength(1); j++)
+                    A[i, j] = clsGeneralFunc.ReadIntData(message2 + "[" + i.ToString() +
+                                                                  ", " + j.ToString() +
+                                                                  "]");
+
+        }
+        // наполнение массива значениями с клавиатуры
+        public static void Fill2DimArrayFromKeyboard(double[,] A, string message1 = "", string message2 = "")
+        {
+            if (message1 != String.Empty) Console.WriteLine(message1);
+            for (int i = 0; i < A.GetLength(0); i++)
+                for (int j = 0; j < A.GetLength(1); j++)
+                    A[i, j] = clsGeneralFunc.ReadDoubleData(message2 + "[" + i.ToString() +
+                                                                  ", " + j.ToString() +
+                                                                  "]");
+
+        }
+
+        // инициализация и заполнение массива
+        public static int[,] Init2DimIntArray(int numRows, int numCols,
+                                                bool needPrintArray = true, //признак, надо ли печатать массив 
+                                                                            //  после его инициализации
+                                                bool fromKeyboard = false, //признак, надо ли считывать массив 
+                                                                           //  с клавиатуры
+                                                                           // true - игнорируются minA и maxA
+                                                bool needFillArray = true, //признак, надо ли заполнять массив
+                                                                           // случайными числами
+                                                                           //fromKeyboard и needFillArray 
+                                                                           // не являются взаимоисклю, тк бывают 
+                                                                           // ситуации, что массив надо только 
+                                                                           // инициализировать, а заполняться
+                                                                           // будет где-то вне по особому алгоритму
+                                                int minA = Int32.MinValue,
+                                                int maxA = Int32.MaxValue)
+        {
+            int rows = numRows;
+            int cols = numCols;
+            if (rows == 0) rows = clsGeneralFunc.ReadIntData("Введите количество строк массива...");
+            if (cols == 0) cols = clsGeneralFunc.ReadIntData("Введите количество столбцов массива...");
+
+            if ((rows > 0) && (cols > 0))
+            {
+                int[,] array = new int[rows, cols];
+                if ((!fromKeyboard) && (needFillArray))
+                {
+                    int minRandom = minA;
+                    if (minRandom == Int32.MinValue)
+                    {
+                        minRandom = clsGeneralFunc.ReadIntData("Введите нижнюю границу для генерации чисел...");
+                    }
+
+                    int maxRandom = maxA;
+                    if (maxRandom == Int32.MaxValue)
+                    {
+                        maxRandom = clsGeneralFunc.ReadIntData("Введите верхнюю границу для генерации чисел...");
+                    }
+
+                    Fill2DimArray(array, minRandom, maxRandom + 1);
+                }
+                if ((fromKeyboard) && (!needFillArray)) Fill2DimArrayFromKeyboard(array, "Введите массив...", "элемент ");
+
+                if (needPrintArray) Print2DimArray(array);
+                return array;
+            }
+            else
+            {
+                rows = 0;//если размер меньше нуля указали или 0, то создаем нулевой массив и ругаемся
+                cols = 0;
+                int[,] array = new int[rows, cols];
+                Console.WriteLine("Количество строк и столбцов массива должно быть больше 0");
+                return array;//null;
+            }
+        }
+
+
+        // инициализация и заполнение массива
+        public static double[,] Init2DimDoubleArray(int numRows, int numCols,
+                                                bool needPrintArray = true, //признак, надо ли печатать массив 
+                                                                            //  после его инициализации
+                                                bool fromKeyboard = false, //признак, надо ли считывать массив 
+                                                                           //  с клавиатуры
+                                                                           // true - игнорируются minA и maxA
+                                                bool needFillArray = true, //признак, надо ли заполнять массив
+                                                                           // случайными числами
+                                                                           //fromKeyboard и needFillArray 
+                                                                           // не являются взаимоисклю, тк бывают 
+                                                                           // ситуации, что массив надо только 
+                                                                           // инициализировать, а заполняться
+                                                                           // будет где-то вне по особому алгоритму
+                                                double minA = -999999999999,
+                                                double maxA = 999999999999)
+        {
+            int rows = numRows;
+            int cols = numCols;
+            if (rows == 0) rows = clsGeneralFunc.ReadIntData("Введите количество строк массива...");
+            if (cols == 0) cols = clsGeneralFunc.ReadIntData("Введите количество столбцов массива...");
+
+            if ((rows > 0) && (cols > 0))
+            {
+                double[,] array = new double[rows, cols];
+                if ((!fromKeyboard) && (needFillArray))
+                {
+                    double minRandom = minA;
+                    if (minRandom == -999999999999)
+                    {
+                        minRandom = clsGeneralFunc.ReadDoubleData("Введите нижнюю границу для генерации чисел...");
+                    }
+
+                    double maxRandom = maxA;
+                    if (maxRandom == 999999999999)
+                    {
+                        maxRandom = clsGeneralFunc.ReadDoubleData("Введите верхнюю границу для генерации чисел...");
+                    }
+
+                    Fill2DimArray(array, minRandom, maxRandom + 1);
+                }
+                if ((fromKeyboard) && (!needFillArray)) Fill2DimArrayFromKeyboard(array, "Введите массив...", "элемент ");
+
+                if (needPrintArray) Print2DimArray(array);
+                return array;
+            }
+            else
+            {
+                rows = 0;//если размер меньше нуля указали или 0, то создаем нулевой массив и ругаемся
+                cols = 0;
+                double[,] array = new double[rows, cols];
+                Console.WriteLine("Количество строк и столбцов массива должно быть больше 0");
+                return array;//null;
+            }
+        }
+        public static int[] GetRowAsArray(int[,] intArr, int numRow)
+        {
+            int[] resArr = new int[intArr.GetLength(1)];
+            for (int i = 0; i < intArr.GetLength(1); i++)
+                resArr[i] = intArr[numRow, i];
+            return resArr;
+        }
+
+        public static void SetArrayAsRow(int[,] intArr, int numRow, int[] rowArr)
+        {
+            for (int i = 0; i < rowArr.Length; i++)
+                intArr[numRow, i] = rowArr[i];
+        }
+
+    }
+}
---
+++ 14 Seminar8/Task056/Program.cs
@@ -0,0 +1,51 @@
+﻿/*Задача 56: Задайте прямоугольный двумерный массив. 
+Напишите программу, которая будет находить строку 
+с наименьшей суммой элементов.
+
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+5 2 6 7
+
+Программа считает сумму элементов в каждой строке и 
+выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+*/
+using Basic2DimArrFunc;
+using Basic1DimArrFunc;
+using GeneralFunc;
+
+public class clsTask56
+{
+    static private int SumOfElement(int[,] intArr, int numRow)
+    {
+        int sum = intArr[numRow, 0];
+        for (int j = 1; j < intArr.GetLength(1); j++)
+            sum += intArr[numRow, j];
+        return sum;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("***********************************************");
+        Console.WriteLine("Задача 56. Строка с минимальной суммой элементов");
+        Console.WriteLine("***********************************************");
+        int[,] arr = clsBasic2DimArrFunc.Init2DimIntArray(numRows: 0,
+                                                           numCols: 0
+                                                           );
+        int[] arrOfSum = new int[arr.GetLength(0)];
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            arrOfSum[i] = SumOfElement(arr, i);
+        }
+        clsBasic1DimArrFunc.PrintArray(arrOfSum, "Сумма элементов массива построчно = ");
+
+        Console.WriteLine($"Наименьшая сумма элементов в строке {clsBasic1DimArrFunc.IndexOfMinIn1DimArr(arrOfSum)}");
+
+    }
+}
