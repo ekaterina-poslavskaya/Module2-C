5f28d3497a5194325c111d99d8690087279dde8a
Ekaterina Poslavskaya <tea84@rambler.ru>
2022-09-03 23:14:20 +0300

    Task064. Homework

---
+++ 17 Seminar9/Task064/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 65: Задайте значения M и N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5 -> "1, 2, 3, 4, 5"
+M = 4; N = 8 -> "4, 6, 7, 8"
+*/
+using GeneralFunc;
+void PrintNumber(int m, int n)
+{
+    if (m == n)
+    {
+        Console.Write($"{m}\t");
+        return;
+    }
+    else
+    {
+        Console.Write($"{m}\t");
+        PrintNumber(m + 1, n);
+    }
+
+}
+
+
+int m = clsGeneralFunc.ReadIntData("Введите нижнюю границу диапазона..");
+int n = clsGeneralFunc.ReadIntData("Введите верхнюю границу диапазона..");
+if (m <= n) PrintNumber(m, n);
+else PrintNumber(n, m);
